aa7186d438a6600c43c8000fb774b1c1b2bf6941
kozincov <32821146+kozincov@users.noreply.github.com>
2021-11-01 19:04:47 +0300

    Add files via upload

---
+++ Функциональное программирование/split-a-string-into-an-array-using-the-split-method.js
@@ -0,0 +1,8 @@
+function splitify(str) {
+  // Only change code below this line
+  let newArr = str.split(/\s|\W/)
+return newArr;
+  // Only change code above this line
+}
+
+console.log(splitify("Hello World,I-am code"));
